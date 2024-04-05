9c38cca0bffd6a8aa9f636e1ca158508cce74d56
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-05 13:56:35 +0200

    :wrench: Update project configuration, setup tailwind and add nuxt-storage module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss'],
+  devtools: { enabled: true },
+  tailwindcss: {
+    cssPath: 'assets/tailwind.css',
+    configPath: 'tailwind.config',
+  }
 })
